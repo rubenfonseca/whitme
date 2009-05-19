7dcb18a0cc647b067c24ff9e7d2da7804624e592
Ruben Fonseca <root@cpan.org>
2009-05-19 21:29:06 +0100

    Initial commit to whitme.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'spec'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'whitme'
+
+Spec::Runner.configure do |config|
+  
+end
